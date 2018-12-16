51406a498c2324819df3d219035c68d6ba721cd2
jasmine <jasminewiggins90@yahoo.com>
2018-12-15 19:29:38 -0600

    2: Added unit test for monthly billing schedule

--- accounting/tests.py
+++ accounting/tests.py
@@ -51,8 +51,19 @@ class TestBillingSchedules(unittest.TestCase):
         self.assertFalse(self.policy.invoices)
         #Invoices should be made when the class is initiated
         pa = PolicyAccounting(self.policy.id)
+
         self.assertEquals(len(self.policy.invoices), 1)
         self.assertEquals(self.policy.invoices[0].amount_due, self.policy.annual_premium)
+
+    def test_monthly_billing_schedule(self):
+        self.policy.billing_schedule = "Monthly"
+        #No invoices currently exist
+        self.assertFalse(self.policy.invoices)
+        #Create invoices for policy id
+        pa = PolicyAccounting(self.policy.id)
+
+        self.assertEquals(len(self.policy.invoices), 12)
+        self.assertEquals(self.policy.invoices[0].amount_due, 100)
 
 
 class TestReturnAccountBalance(unittest.TestCase):
